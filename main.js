547a55f95d5a776966a75212bb75d881d19213a9
ed <Cabrale234@gmail.com>
2021-07-19 22:18:47 -0400

    style and js

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function addItem() {
+        const oneItem = document.getElementById("item").value;
+       console.log("oneItem");
+}
+
+function select() {
+    document.getElementById('.add').addEventListener('click', addItem);
+}
+select();
+
+
+
+
+  
